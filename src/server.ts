8066ecc7023567bd8d8bbf7a3243eb11f084d56f
chain <klaus.mika1052@gmail.com>
2023-07-11 11:51:03 -0300

    feat: integrade with whisper API and transcribe audio

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 const recorder = require('node-record-lpcm16')
 import fs from 'fs'
+import FormData from 'form-data'
+import axios from 'axios'
+import 'dotenv/config'
 
 function record(seconds: number) {
   const file = fs.createWriteStream('audio.wav', { encoding: 'binary'})
@@ -13,4 +16,26 @@ function record(seconds: number) {
   }, seconds * 1000)
 }
 
-record(5)
+
+async function transcribeAudio(filename: string) {
+  const readStream = fs.createReadStream(filename)
+
+  const formData = new FormData()
+  formData.append('file', readStream, 'audio.wav')
+  formData.append('model', 'whisper-1')
+
+  const transcript = await axios.post('https://api.openai.com/v1/audio/transcriptions', formData, {
+    headers: {
+      ...formData.getHeaders(),
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    }
+  })
+  const text = transcript.data.text
+  console.log(text)
+
+
+
+
+}
+
+transcribeAudio('audio.wav')
